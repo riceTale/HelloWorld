a77bd1b9de971c18d3b46d60ed7b93ffed4b48cc
Nathan <fny66666@gmail.com>
2024-04-17 12:18:59 -0400

    Just the connection part
    nathandb.firstCollection

---
+++ MongoDriver/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main( String[] args ) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb://localhost:27017";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("nathandb");
+            MongoCollection<Document> collection = database.getCollection("firstCollection");
+            Document doc = collection.find(eq("somekey", "somevalue")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
